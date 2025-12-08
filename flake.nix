a1e80808dd69d8d8132e2e9f96aa8981dd4bba9d
Guilhem Saurel <guilhem.saurel@laas.fr>
2025-12-08 15:27:10 +0100

    flake: global override

--- flake.nix
+++ flake.nix
@@ -8,83 +8,93 @@
 
   outputs =
     inputs:
-    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = inputs.nixpkgs.lib.systems.flakeExposed;
-      perSystem =
-        {
-          pkgs,
-          self',
-          ...
-        }:
-        {
-          apps = {
-            default = {
-              type = "app";
-              program = pkgs.python3.withPackages (_: [ self'.packages.default ]);
-            };
-            jupyter = {
-              type = "app";
-              program = pkgs.writeShellApplication {
-                name = "jupyter-crocoddyl";
-                text = "jupyter lab";
-                runtimeInputs = [
-                  (pkgs.python3.withPackages (p: [
-                    p.jupyterlab
-                    p.meshcat
-                    self'.packages.default
-                  ]))
-                ];
-              };
-            };
-          };
-          devShells.default = pkgs.mkShell {
-            inputsFrom = [ self'.packages.default ];
-            packages = with pkgs; [
-              ffmpeg
-              (python3.withPackages (p: [
-                p.tomlkit
-                p.matplotlib
-                p.nbconvert
-                p.nbformat
-                p.ipykernel
-              ]))
-            ];
-            shellHook = ''
-              export PATH=${pkgs.ffmpeg}/bin:$PATH
-            '';
-          };
-          packages = {
-            default = self'.packages.crocoddyl;
-            crocoddyl = pkgs.python3Packages.crocoddyl.overrideAttrs (super: {
-              patches = [ ];
-              src = pkgs.lib.fileset.toSource {
-                root = ./.;
-                fileset = pkgs.lib.fileset.unions [
-                  ./benchmark
-                  ./bindings
-                  ./CMakeLists.txt
-                  ./crocoddyl.cmake
-                  ./doc
-                  ./examples
-                  ./include
-                  ./notebooks
-                  ./package.xml
-                  ./src
-                  ./unittest
-                ];
-              };
-              checkInputs = (super.checkInputs or [ ]) ++ [
-                pkgs.python3Packages.nbconvert
-                pkgs.python3Packages.nbformat
-                pkgs.python3Packages.ipykernel
-                pkgs.python3Packages.matplotlib
-                pkgs.ffmpeg
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } (
+      { lib, self, ... }:
+      {
+        systems = inputs.nixpkgs.lib.systems.flakeExposed;
+        flake.overlays.default = final: prev: {
+          crocoddyl = prev.crocoddyl.overrideAttrs (super: {
+            patches = [ ];
+            src = lib.fileset.toSource {
+              root = ./.;
+              fileset = lib.fileset.unions [
+                ./benchmark
+                ./bindings
+                ./CMakeLists.txt
+                ./crocoddyl.cmake
+                ./doc
+                ./examples
+                ./include
+                ./notebooks
+                ./package.xml
+                ./src
+                ./unittest
               ];
-              preCheck = ''
+            };
+            checkInputs = (super.checkInputs or [ ]) ++ [
+              final.python3Packages.nbconvert
+              final.python3Packages.nbformat
+              final.python3Packages.ipykernel
+              final.python3Packages.matplotlib
+              final.ffmpeg
+            ];
+            preCheck = ''
+              export PATH=${final.ffmpeg}/bin:$PATH
+            '';
+          });
+        };
+        perSystem =
+          {
+            pkgs,
+            self',
+            system,
+            ...
+          }:
+          {
+            _module.args.pkgs = import inputs.nixpkgs {
+              inherit system;
+              overlays = [ self.overlays.default ];
+            };
+            apps = {
+              default = {
+                type = "app";
+                program = pkgs.python3.withPackages (_: [ self'.packages.default ]);
+              };
+              jupyter = {
+                type = "app";
+                program = pkgs.writeShellApplication {
+                  name = "jupyter-crocoddyl";
+                  text = "jupyter lab";
+                  runtimeInputs = [
+                    (pkgs.python3.withPackages (p: [
+                      p.jupyterlab
+                      p.meshcat
+                      self'.packages.default
+                    ]))
+                  ];
+                };
+              };
+            };
+            devShells.default = pkgs.mkShell {
+              inputsFrom = [ self'.packages.default ];
+              packages = with pkgs; [
+                ffmpeg
+                (python3.withPackages (p: [
+                  p.matplotlib
+                  p.nbconvert
+                  p.nbformat
+                  p.ipykernel
+                ]))
+              ];
+              shellHook = ''
                 export PATH=${pkgs.ffmpeg}/bin:$PATH
               '';
-            });
+            };
+            packages = {
+              default = self'.packages.crocoddyl;
+              crocoddyl = pkgs.python3Packages.crocoddyl;
+            };
           };
-        };
-    };
+      }
+    );
 }
